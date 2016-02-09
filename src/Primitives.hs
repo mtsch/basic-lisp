4727f76685c39a43a5204388df463f3426ef679f
mtsch <matijacufar@gmail.com>
2016-02-09 12:13:49 +0100

    Working with lists and comparing integers

--- src/Primitives.hs
+++ src/Primitives.hs
@@ -9,11 +9,14 @@ import           SExpr
 airityError :: SExpr
 airityError = Err "Wrong airity!"
 
+expectingError :: String -> SExpr
+expectingError s = Err $ "Function expecting " ++ s ++ "!"
+
 -- Two argument fun on Int to two or more argument primitive
-intintPrimitive :: (Int -> Int -> Int) -> [SExpr] -> SExpr
-intintPrimitive _ []       = airityError
-intintPrimitive _ [_]      = airityError
-intintPrimitive fun (e:es) =
+intintPrim :: (Int -> Int -> Int) -> [SExpr] -> SExpr
+intintPrim _ []       = airityError
+intintPrim _ [_]      = airityError
+intintPrim fun (e:es) =
     case e of
       (Integer i) -> aux i es
           where
@@ -23,41 +26,53 @@ intintPrimitive fun (e:es) =
                   (first:rest) -> case first of
                                     (Integer x) -> let acc' = fun acc x
                                                    in aux acc' rest
-                                    _ -> Err "Function expecting integer args!"
-      _ -> Err "Function expecting integer args!"
+                                    _ -> expectingError "two integers"
+      _ -> expectingError "two integers"
 
--- Binary comparison functions
-comparisonFun :: (SExpr -> SExpr -> Bool) -> [SExpr] -> SExpr
-comparisonFun fun [exp1, exp2] = Bool $ fun exp1 exp2
-comparisonFun _ _              = airityError
+-- Equal an not equal function wrapper.
+eqneqPrim :: (SExpr -> SExpr -> Bool) -> [SExpr] -> SExpr
+eqneqPrim fun [exp1, exp2] = Bool $ fun exp1 exp2
+eqneqPrim _ _              = airityError
 
-headPrimitive :: [SExpr] -> SExpr
-headPrimitive [List []] = Err "Function expecting non-empty list!"
-headPrimitive [List l]  = head l
-headPrimitive [_]       = Err "Function expecting list!"
-headPrimitive _         = airityError
+intComparisonPrim :: (Int -> Int -> Bool) -> [SExpr] -> SExpr
+intComparisonPrim fun [Integer i1, Integer i2] = Bool $ fun i1 i2
+intComparisonPrim _ [_, _]                     = expectingError "two integers"
+intComparisonPrim _ _                          = airityError
 
-tailPrimitive :: [SExpr] -> SExpr
-tailPrimitive [List []] = Err "Function expecting non-empty list!"
-tailPrimitive [List l] = List $ tail l
-tailPrimitive [_]      = Err "Function expecting list!"
-tailPrimitive _        = airityError
+-- List head.
+headPrim :: [SExpr] -> SExpr
+headPrim [List []] = expectingError "non-empty list"
+headPrim [List l]  = head l
+headPrim [_]       = expectingError "list"
+headPrim _         = airityError
 
-consPrimitive :: [SExpr] -> SExpr
-consPrimitive [val, List l] = List $ val : l
-consPrimitive [_, _]        = Err "Function expecting value and list!"
-consPrimitive _             = airityError
+-- List tail.
+tailPrim :: [SExpr] -> SExpr
+tailPrim [List []] = expectingError "non-empty list"
+tailPrim [List l]  = List $ tail l
+tailPrim [_]       = expectingError "list"
+tailPrim _         = airityError
+
+-- Cons for lists.
+consPrim :: [SExpr] -> SExpr
+consPrim [val, List l] = List $ val : l
+consPrim [_, _]        = expectingError "value and list"
+consPrim _             = airityError
 
 -- Environment containing primitives.
 primitiveEnv :: Environment
-primitiveEnv = Map.fromList [ ("+",    Primitive $ intintPrimitive (+))
-                            , ("-",    Primitive $ intintPrimitive (-))
-                            , ("*",    Primitive $ intintPrimitive (*))
-                            , ("div",  Primitive $ intintPrimitive div)
-                            , ("rem",  Primitive $ intintPrimitive rem)
-                            , ("=",    Primitive $ comparisonFun (==))
-                            , ("not=", Primitive $ comparisonFun (/=))
-                            , ("head", Primitive $ headPrimitive)
-                            , ("tail", Primitive $ tailPrimitive)
-                            , ("cons", Primitive $ consPrimitive)
+primitiveEnv = Map.fromList [ ("+",    Primitive $ intintPrim (+))
+                            , ("-",    Primitive $ intintPrim (-))
+                            , ("*",    Primitive $ intintPrim (*))
+                            , ("div",  Primitive $ intintPrim div)
+                            , ("rem",  Primitive $ intintPrim rem)
+                            , ("=",    Primitive $ eqneqPrim (==))
+                            , ("not=", Primitive $ eqneqPrim (/=))
+                            , (">",    Primitive $ intComparisonPrim (>))
+                            , (">=",   Primitive $ intComparisonPrim (>=))
+                            , ("<",    Primitive $ intComparisonPrim (<))
+                            , ("<=",   Primitive $ intComparisonPrim (<=))
+                            , ("head", Primitive $ headPrim)
+                            , ("tail", Primitive $ tailPrim)
+                            , ("cons", Primitive $ consPrim)
                             ]

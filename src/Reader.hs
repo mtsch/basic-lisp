760520b8d29774b7d71065584abf225a66db19f6
mtsch <matijacufar@gmail.com>
2016-02-16 20:56:11 +0100

    Applicative reader

--- src/Reader.hs
+++ src/Reader.hs
@@ -3,7 +3,8 @@ module Reader
     ( readSExpr )
 where
 
-import Text.Parsec hiding (spaces, space)
+import Control.Applicative
+import Text.Parsec hiding (spaces, space, (<|>), many)
 
 import SExpr
 
@@ -17,56 +18,44 @@ space = oneOf " \t\n\r,"
 
 -- Skip comment
 comment :: Parsec String () ()
-comment = (do _ <- char ';'
-              skipMany $ noneOf "\n"
-              _ <- char '\n'
-              return ()) <?> ""
+comment = (char ';' *> skipMany (noneOf "\n") *> char '\n' *> pure ()) <?> ""
 
 -- Skip spaces.
 spaces :: Parsec String () ()
-spaces = skipMany space >> skipMany comment
+spaces = skipMany space *> skipMany comment
 
 -- Parse an atom.
 atom :: Parsec String () SExpr
-atom = do first <- symbol <|> letter
-          rest  <- many (symbol <|> alphaNum)
-          let name = first : rest
-          return $ case name of
-                     "true"  -> Bool True
-                     "false" -> Bool False
-                     "nil"   -> Nil
-                     _       -> Atom name
+atom = liftA pick $ (:) <$> (symbol <|> letter) <*> many (symbol <|> alphaNum)
+       where
+         pick name =
+             case name of
+               "true"  -> Bool True
+               "false" -> Bool False
+               "nil"   -> Nil
+               _       -> Atom name
 
--- Parse a container delimited by l and r.
--- l and r can't start or end with valid symbols!
-container :: (String, String) -> Parsec String () [SExpr]
-container (l, r) = do spaces
-                      _ <- string l
-                      spaces
-                      contents <- expr `sepEndBy` spaces
-                      _ <- string r
-                      spaces
-                      return contents
+-- Parse a container separated by l and r
+container :: String -> String -> Parsec String () [SExpr]
+container l r = left *> expr `sepEndBy` spaces <* right
+    where
+      left  = spaces *> string l *> spaces
+      right = spaces <* string r <* spaces
 
 -- Parse a list.
 list :: Parsec String () SExpr
-list = List <$> container ("(", ")")
+list = List <$> container "(" ")"
 
 -- Parse a quoted list.
 quotList :: Parsec String () SExpr
-quotList = List <$> (container ("'(", ")") >>= \l ->
-                         return $ Atom "quote" : [List l])
+quotList = liftA (\l -> List $ Atom "quote" : [List l]) $ container "'(" ")"
 
 -- Parse a number.
 int :: Parsec String () SExpr
 int = (Int . read) <$> many1 digit
 
--- Parse a string.
 str :: Parsec String () SExpr
-str = do _ <- char '"'
-         value <- many $ noneOf "\""
-         _ <- char '"'
-         (return . String) value
+str = liftA String $ char '"' *> many (noneOf "\"") <* char '"'
 
 -- Parse an expression.
 expr :: Parsec String () SExpr
@@ -77,5 +66,4 @@ readSExpr :: String -> [SExpr]
 readSExpr text = either (\e -> [Err $ "Parse error in " ++ show e])
                         id (parse exprs "" text)
     where
-      exprs = do spaces
-                 expr `sepEndBy` spaces
+      exprs = spaces *> expr `sepEndBy` spaces
